a13dc6f5cf5dc9db15c2a94cca9f054c01f65b2e
Michelle Tan <michelle0223@gmail.com>
2023-10-02 17:28:38 -0700

    need help trouble shoot

---
+++ controllers/CustomerMessage.js
@@ -0,0 +1,8 @@
+'use strict'
+var express = require('express')
+// GET THE INFORMATION FROM ROUTES, AND PARSE IT, save to mdeol
+const CreateCustomerMessage = (req, res) => {
+    console.log({req: req.body});
+};
+
+module.exports = CreateCustomerMessage;
