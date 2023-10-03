8a2b970821c62bd640fbe3567efe3ad349136952
Michelle Tan <michelle0223@gmail.com>
2023-10-02 17:28:38 -0700

    need help trouble shoot

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const CreateCustomerMessage = require("../controllers/customerMessage").default
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// /* POST create customer and text */
+// //i'm going to pass the controller
+router.post("/create-customer-text", CreateCustomerMessage);
 
 module.exports = router;
