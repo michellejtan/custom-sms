72058fee310a28eeefee727b139c121dec0f2bd3
Michelle Tan <michelle0223@gmail.com>
2023-10-11 23:07:21 +0000

    delete of files, to readd

--- routes/customer.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const customerController = require('../controllers/customer')
-
-router.post('/', customerController);
-
-module.exports = router;
