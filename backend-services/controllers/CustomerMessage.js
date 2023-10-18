450de403d03435fb8ea9ed379197dd8e4914a4fc
Michelle Tan <michelle0223@gmail.com>
2023-10-18 16:12:02 -0700

    Changes for the button

--- backend-services/controllers/CustomerMessage.js
+++ backend-services/controllers/CustomerMessage.js
@@ -8,12 +8,17 @@ const CreateCustomerMessage = async(req, res) => {
     console.log(`Logging request: ${req.body}`+JSON.stringify(req.body));
     // console.log({req: req.body});
     // console.log({model: {CustomerMessage}});
+    //validation
+    if(!req.body.phoneNumber || !req.body.message){
+        res.status(400).send("Missing fields");
+    }
+
     await CustomerMessage.create({
         phoneNumber: req.body.phoneNumber, 
         message: req.body.message,
     });
 
-    res.status(200).send("Successfully saved.");
+    res.status(200).send("Successfully saved!");
     // res.send({Response: 200});
 };
 
--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,8 @@ function App() {
   // Memory Storage
   const [phoneNumberState, setPhoneNumber] =useState("");
   const [messageState, setMessage] =useState("");
-  // const[Error, setError] =useState("");
+  const[error, setError] =useState("null");
+  const[successfullResponse, setSuccessfullResponse] =useState("null");
 
   // get the value that was typed!
   const updatePhoneNumber = (e) => {
@@ -25,6 +26,7 @@ function App() {
   }
 
   const submitText =()=>{
+    // everytime to click button, send information is gonna reset these 2 fields to an empty value
     // setError(null);
     // setSuccessfullResponse(null);
 
@@ -33,25 +35,32 @@ function App() {
     // collect the data from the State
     // send the data to the backend
 
-    axios.post("http://localhost:3000/create-customer-text",{
+    // main port
+    axios.post("http://localhost:3000/create-customer-text", {
       // axios.post("http://localhost:3000/create-customer-text",{
       phoneNumber:phoneNumberState,
       message:messageState
     })
-    // .then((response)=>{
-    //   console.log("MESSAGE RETURNED: ", response.data);
-    //   setSuccessfullResponse(response.data);
-    // })
-    // .catch((e) =>{
-    //   setError(e.message);
-    // });
+    .then((response)=>{
+      // console.log(response);
+      console.log("MESSAGE RETURNED: ", response.data);
+      setSuccessfullResponse(response.data);
+    })
+    .catch((e) =>{
+      setError(e.message);
+    });
 
     // collect data
   };
+
+  // get all the messages
+
   //do the same thing[const unas, or function]
   //function submit() {}
   return (
     <div className="App">
+      {error && <div>ERROR SENDING THE DATA: {error}</div>}
+      {successfullResponse && <div>The message was {successfullResponse}</div>}
       <header className="App-header">
        <h1>Marketing Campaign</h1>
        <p>Phone number</p>
